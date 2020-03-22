b98bdadc156b8ba73638f925038418bd9a5f75ac
Jon Whitehead <jsiwhitehead@gmail.com>
2020-03-22 17:06:16 +0000

    Added root nodes and event properties

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ import { fromJs } from 'maraca';
 //   ),
 // );
 
-const toJs = data => {
+const toJs = (data) => {
   if (data.type === 'value') return data.value;
   return data.value
     .toPairs()
@@ -42,7 +42,7 @@ const toIndex = (v: string) => {
   const n = parseFloat(v);
   return !isNaN(v as any) && !isNaN(n) && n === Math.floor(n) && n > 0 && n;
 };
-const unpack = value => {
+const unpack = (value) => {
   const result = { values: {} as any, indices: [] as any[] };
   value.toPairs().forEach(({ key, value }) => {
     if (key.type !== 'box') {
@@ -54,12 +54,12 @@ const unpack = value => {
   return result;
 };
 
-const isObject = x => Object.prototype.toString.call(x) === '[object Object]';
+const isObject = (x) => Object.prototype.toString.call(x) === '[object Object]';
 const diffObjs = (next, prev) => {
   const result = {};
   Array.from(
     new Set([...Object.keys(next), ...Object.keys(prev || {})]),
-  ).forEach(k => {
+  ).forEach((k) => {
     if (next[k] !== (prev || {})[k]) {
       result[k] = isObject(next[k])
         ? diffObjs(next[k], (prev || {})[k])
@@ -69,9 +69,13 @@ const diffObjs = (next, prev) => {
   return result;
 };
 const applyObj = (target, obj) => {
-  Object.keys(obj).forEach(k => {
+  Object.keys(obj).forEach((k) => {
     if (!isObject(obj[k])) {
-      target[k] = obj[k] === undefined ? null : obj[k];
+      if (['svg', 'path'].includes(target.tagName?.toLowerCase())) {
+        target.setAttribute(k, obj[k] === undefined ? null : obj[k]);
+      } else {
+        target[k] = obj[k] === undefined ? null : obj[k];
+      }
     } else {
       applyObj(target[k], obj[k]);
     }
@@ -79,22 +83,95 @@ const applyObj = (target, obj) => {
   return target;
 };
 
-const createNode = type =>
-  type === 'text' ? document.createTextNode('') : document.createElement(type);
+const createNode = (type) =>
+  type === 'text'
+    ? document.createTextNode('')
+    : ['svg', 'path'].includes(type)
+    ? document.createElementNS('http://www.w3.org/2000/svg', type)
+    : document.createElement(type);
 
-const getNodeInfo = data => {
+const getNodeInfo = (data) => {
   if (data.type === 'value') return { type: 'text', props: data.value };
   const {
     values: { ['']: type, ...props },
     indices,
   } = unpack(data.value);
-  return { type: type?.type === 'value' ? type.value : 'div', props, indices };
+  const typeValue = type?.type === 'value' ? type.value : 'div';
+  return {
+    type: typeValue.startsWith(' ') ? typeValue.slice(1) || 'div' : typeValue,
+    root: typeValue.startsWith(' '),
+    props,
+    indices,
+  };
 };
 
-const getNode = (data, prev) => {
+class Root {
+  root;
+  map = new Map();
+  constructor(root) {
+    this.root = root;
+  }
+  get(node, base = false) {
+    if (!this.map.has(node)) {
+      const rootNode = document.createElement('div');
+      rootNode.style.zIndex = '0';
+      if (base) {
+        rootNode.style.position = 'relative';
+        rootNode.style.height = '100%';
+      } else {
+        rootNode.style.position = 'fixed';
+        rootNode.style.top = '0px';
+        rootNode.style.left = '0px';
+      }
+      this.root.appendChild(rootNode);
+      this.map.set(node, rootNode);
+    }
+    return this.map.get(node);
+  }
+  remove(node) {
+    if (this.map.has(node)) {
+      this.root.removeChild(this.map.get(node));
+      this.map.delete(node);
+    }
+  }
+}
+
+const eventProperties = {
+  mouse: [
+    'altKey',
+    'button',
+    'buttons',
+    'clientX',
+    'clientY',
+    'ctrlKey',
+    'metaKey',
+    'movementX',
+    'movementY',
+    'region',
+    'screenX',
+    'screenY',
+    'shiftKey',
+  ],
+  keyboard: [
+    'altKey',
+    'code',
+    'ctrlKey',
+    'isComposing',
+    'key',
+    'locale',
+    'location',
+    'metaKey',
+    'repeat',
+    'shiftKey',
+  ],
+};
+
+const pick = (obj, keys) =>
+  keys.reduce((res, k) => ({ ...res, [k]: obj[k] }), {});
+
+const getNode = (root, { data, info }, prev) => {
   if (prev?.__data === data) return prev;
 
-  const info = getNodeInfo(data);
   const node =
     info.type === prev?.nodeName.replace('#', '').toLowerCase()
       ? prev
@@ -108,63 +185,82 @@ const getNode = (data, prev) => {
         (res, k) => ({ ...res, [k]: toJs(info.props[k]) }),
         {},
       ),
-      onmouseenter: hover?.push && (() => hover?.push(fromJs(true))),
+      onmouseenter:
+        hover?.push &&
+        ((e) => hover?.push(fromJs(pick(e, eventProperties.mouse)))),
       onmouseleave: hover?.push && (() => hover?.push(fromJs(false))),
       onmousedown:
         click?.push &&
-        (e => {
-          if (e.button === 0) click?.push(fromJs(null));
+        ((e) => {
+          if (e.button === 0) {
+            click?.push(fromJs(pick(e, eventProperties.mouse)));
+          }
         }),
       onkeypress:
         enter?.push &&
-        (e => {
-          if (e.keyCode === 13) enter?.push(fromJs(null));
+        ((e) => {
+          if (e.keyCode === 13)
+            enter?.push(fromJs(pick(e, eventProperties.keyboard)));
         }),
       onfocus: focus?.push && (() => focus?.push(fromJs(true))),
       onblur: focus?.push && (() => focus?.push(fromJs(false))),
       oninput:
         otherProps.value?.push &&
-        (e => otherProps.value?.push(fromJs(e.target.value))),
+        ((e) => otherProps.value?.push(fromJs(e.target.value))),
     } as any;
-
-    // ? {
-    //   overflow: 'hidden',
-    //   cursor: 'pointer',
-    //   userSelect: 'none',
-    //   WebkitUserSelect: 'none',
-    //   MozUserSelect: 'none',
-    //   msUserSelect: 'none',
-    // }
 
     if (focus?.value) setTimeout(() => node.focus());
 
     const diff = diffObjs(props, node.__props || {});
     applyObj(node, diff);
     node.__props = props;
-    updateChildren(node, info.indices);
+    updateChildren(root, node, info.indices);
   }
   node.__data = data;
   return node;
 };
 
-const updateChildren = (node, indices) => {
+const updateChildrenSet = (root, node, indices) => {
   const children = [...node.childNodes];
   for (let i = 0; i < Math.max(children.length, indices.length); i++) {
+    const prev = children[i];
     if (!indices[i]) {
-      node.removeChild(children[i]);
+      root.remove(prev);
+      node.removeChild(prev);
     } else {
-      const prev = children[i];
-      const next = getNode(indices[i], prev);
-      if (!prev) node.appendChild(next);
-      else if (next !== prev) node.replaceChild(next, prev);
+      const next = getNode(root, indices[i], prev);
+      if (!prev) {
+        node.appendChild(next);
+      } else if (next !== prev) {
+        root.remove(prev);
+        node.replaceChild(next, prev);
+      }
     }
   }
 };
+const updateChildren = (root, node, indices) => {
+  const mappedIndices = indices.map((data) => ({
+    data,
+    info: getNodeInfo(data),
+  }));
+  updateChildrenSet(
+    root,
+    node,
+    mappedIndices.filter((x) => !x.info.root),
+  );
+  const rootIndices = mappedIndices.filter((x) => x.info.root);
+  if (rootIndices.length === 0) root.remove(node);
+  else updateChildrenSet(root, root.get(node), rootIndices);
+};
 
-export default node => data => {
-  if (data) {
-    updateChildren(node, [data]);
-  } else {
-    while (node.lastChild) node.removeChild(node.lastChild);
-  }
+export default (node) => {
+  const root = new Root(node);
+  const rootNode = root.get(node, true);
+  return (data) => {
+    if (data) {
+      updateChildren(root, rootNode, [data]);
+    } else {
+      while (node.lastChild) node.removeChild(node.lastChild);
+    }
+  };
 };
